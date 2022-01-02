db615f3240b04b56bd8f1c0b7dc43fe98340aee5
KaptainWutax <kaptainwutax@gmail.com>
2022-01-02 11:15:08 -0500

    moved capability mixin

---
+++ forge/src/main/java/iskallia/ispawner/forge/mixin/MixinCapabilityProvider.java
@@ -0,0 +1,31 @@
+package iskallia.ispawner.forge.mixin;
+
+import iskallia.ispawner.block.entity.SurvivalSpawnerBlockEntity;
+import net.minecraft.util.math.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityProvider;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CapabilityProvider.class)
+public abstract class MixinCapabilityProvider implements ICapabilityProvider {
+
+	@Inject(method = "getCapability", at = @At("HEAD"), remap = false)
+	public <T> void getCapability(Capability<T> cap, Direction side, CallbackInfoReturnable<LazyOptional<T>> ci) {
+		if((Object)this instanceof SurvivalSpawnerBlockEntity) {
+			if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+				SurvivalSpawnerBlockEntity entity = (SurvivalSpawnerBlockEntity)(Object)this;
+				ci.setReturnValue(LazyOptional.of(() -> new InvWrapper(entity.getInventory())).cast());
+			}
+
+			ci.setReturnValue(LazyOptional.empty());
+		}
+	}
+
+}
--- forge/src/main/java/iskallia/ispawner/forge/mixin/MixinSpawnerBlockEntity.java
+++ forge/src/main/java/iskallia/ispawner/forge/mixin/MixinSpawnerBlockEntity.java
@@ -1,49 +1,16 @@
 package iskallia.ispawner.forge.mixin;
 
-import iskallia.ispawner.block.entity.BaseBlockEntity;
 import iskallia.ispawner.block.entity.SpawnerBlockEntity;
-import iskallia.ispawner.block.entity.SurvivalSpawnerBlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.inventory.InventoryChangedListener;
-import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.util.Tickable;
 import net.minecraft.util.math.Box;
-import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.extensions.IForgeTileEntity;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.wrapper.InvWrapper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
-import javax.annotation.Nonnull;
+@Mixin(SpawnerBlockEntity.class)
+public abstract class MixinSpawnerBlockEntity implements IForgeTileEntity {
 
-@Mixin(value = SpawnerBlockEntity.class, priority = 1001)
-public abstract class MixinSpawnerBlockEntity extends BaseBlockEntity implements Tickable, NamedScreenHandlerFactory, InventoryChangedListener {
-
-	public MixinSpawnerBlockEntity(BlockEntityType<?> type) {
-		super(type);
-	}
-
-	/**
-	 * @author iSpawners (Iskallia)
-	 */
-	@Overwrite
+	@Override
 	public Box getRenderBoundingBox() {
 		return IForgeTileEntity.INFINITE_EXTENT_AABB;
-	}
-
-	/**
-	 * @author iSpawners (Iskallia)
-	 */
-	@Overwrite
-	public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap) {
-		if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && (Object)this instanceof SurvivalSpawnerBlockEntity) {
-			SurvivalSpawnerBlockEntity entity = (SurvivalSpawnerBlockEntity)(Object)this;
-			return LazyOptional.of(() -> new InvWrapper(entity.getInventory())).cast();
-		}
-
-		return LazyOptional.empty();
 	}
 
 }
