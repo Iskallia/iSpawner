b51e5510ebc3b3589c73c2ff4319a441dbf35804
KaptainWutax <kaptainwutax@gmail.com>
2023-02-07 13:05:51 -0500

    fix survival spawner dropping incorrectly

--- common/src/main/java/iskallia/ispawner/block/SurvivalSpawnerBlock.java
+++ common/src/main/java/iskallia/ispawner/block/SurvivalSpawnerBlock.java
@@ -6,10 +6,14 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SidedInventory;
+import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
+
+import java.util.logging.Level;
 
 public class SurvivalSpawnerBlock extends SpawnerBlock {
 
@@ -31,6 +35,20 @@ public class SurvivalSpawnerBlock extends SpawnerBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
         return checkType(type, ModBlocks.Entities.SURVIVAL_SPAWNER.get(), SurvivalSpawnerBlockEntity::tick);
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if(!state.isOf(newState.getBlock())) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+
+            if(blockEntity instanceof SurvivalSpawnerBlockEntity spawner) {
+                ItemScatterer.spawn(world, pos, spawner.input);
+                world.updateComparators(pos, this);
+            }
+
+            super.onStateReplaced(state, world, pos, newState, moved);
+        }
     }
 
 }
